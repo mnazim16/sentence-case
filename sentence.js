324535c175463ee424d48553c559471cf5630b45
Mohamed Nazim <nazim.16@gmail.com>
2016-09-24 11:39:51 +0530

    Create sentence.js

---
+++ sentence.js
@@ -0,0 +1,17 @@
+function change(charm) {
+    var string = eval(charm), change = "", newstring = string.split("."), tochange;
+    for (var i = 0; i < newstring.length; i++) {
+        tochange = newstring[i].trim().charAt(0);
+        newstring[i] = newstring[i].replace(tochange, tochange.toUpperCase());
+    }
+    change = newstring.join(".");
+    return change;
+}
+
+var x = document.getElementsByTagName("P");
+var i;
+for (i = 0; i < x.length; i++) {
+var divi =document.getElementsByTagName('p')[i].innerHTML;
+var text = change('divi');
+document.getElementsByTagName("p")[i].innerHTML = text;
+}
